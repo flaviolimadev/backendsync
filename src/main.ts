959319134c1ffb1b2c16aef2fac19a7ada3cc067
Flavio Lima <ctrlserr@gmail.com>
2025-03-23 18:21:41 -0300

    DELET CORS

--- src/main.ts
+++ src/main.ts
@@ -6,5 +6,12 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+
+  // Permitir CORS
+  app.enableCors({
+    origin: '*', // ou '*' para liberar geral (não recomendado em produção)
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
 }
 bootstrap();
